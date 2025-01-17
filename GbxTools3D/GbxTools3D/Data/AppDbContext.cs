2137e9241ca898c20fe899358ec51b0bc852ac0b
Petr Pivoňka <petrpiv1@gmail.com>
2025-01-17 03:35:34 +0100

    Add web server fanciness

---
+++ GbxTools3D/GbxTools3D/Data/AppDbContext.cs
@@ -0,0 +1,7 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace GbxTools3D.Data;
+
+public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
+{
+}
--- GbxTools3D/GbxTools3D/Program.cs
+++ GbxTools3D/GbxTools3D/Program.cs
@@ -1,5 +1,10 @@
-using GbxTools3D.Client.Pages;
 using GbxTools3D.Components;
+using GbxTools3D.Data;
+using Microsoft.AspNetCore.CookiePolicy;
+using Microsoft.AspNetCore.ResponseCompression;
+using Microsoft.EntityFrameworkCore;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Trace;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,7 +13,55 @@ builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents()
     .AddInteractiveWebAssemblyComponents();
 
+builder.Services.AddResponseCompression(options =>
+{
+    options.EnableForHttps = true;
+    options.Providers.Add<BrotliCompressionProvider>();
+    options.Providers.Add<GzipCompressionProvider>();
+});
+
+builder.Services.AddDbContextFactory<AppDbContext>(options =>
+{
+    //var connectionStr = builder.Configuration.GetConnectionString("DefaultConnection");
+    //options.UseMySql(connectionStr, ServerVersion.AutoDetect(connectionStr));
+    options.UseInMemoryDatabase("GbxTools3D");
+});
+
+builder.Services.AddOpenTelemetry()
+    .WithMetrics(options =>
+    {
+        options
+            .AddHttpClientInstrumentation()
+            .AddRuntimeInstrumentation()
+            .AddProcessInstrumentation()
+            .AddOtlpExporter();
+
+        options.AddMeter("System.Net.Http");
+    })
+    .WithTracing(options =>
+    {
+        if (builder.Environment.IsDevelopment())
+        {
+            options.SetSampler<AlwaysOnSampler>();
+        }
+
+        options
+            .AddHttpClientInstrumentation()
+            .AddOtlpExporter();
+    });
+builder.Services.AddMetrics();
+
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+
+    if (dbContext.Database.IsRelational())
+    {
+        dbContext.Database.Migrate();
+    }
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -24,8 +77,19 @@ else
 
 app.UseHttpsRedirection();
 
-
 app.UseAntiforgery();
+
+app.UseCookiePolicy(new CookiePolicyOptions
+{
+    MinimumSameSitePolicy = SameSiteMode.Lax,
+    Secure = CookieSecurePolicy.Always,
+    HttpOnly = HttpOnlyPolicy.Always
+});
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseResponseCompression();
+}
 
 app.MapStaticAssets();
 app.MapRazorComponents<App>()
