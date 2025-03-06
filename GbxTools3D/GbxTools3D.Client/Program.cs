79c7e5166829e7ade6d1ec11956025fdb8583482
Petr Pivoňka <petrpiv1@gmail.com>
2025-03-06 22:32:31 +0100

    Redo loading, add player

--- GbxTools3D/GbxTools3D.Client/Program.cs
+++ GbxTools3D/GbxTools3D.Client/Program.cs
@@ -1,11 +1,13 @@
 using GBX.NET;
 using GBX.NET.LZO;
+using GBX.NET.ZLib;
 using GbxTools3D.Client.Services;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
 Gbx.LZO = new MiniLZO();
+Gbx.ZLib = new ZLib();
 
 builder.Services.AddScoped(sp =>
     new HttpClient
