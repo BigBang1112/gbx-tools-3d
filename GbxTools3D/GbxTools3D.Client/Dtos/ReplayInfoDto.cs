8e24407192294f7a766cfc82c57773bba53e503c
Petr Pivoňka <petrpiv1@gmail.com>
2025-07-24 01:04:45 +0200

    Fix beaten tmx replays issue

--- GbxTools3D/GbxTools3D.Client/Dtos/ReplayInfoDto.cs
+++ GbxTools3D/GbxTools3D.Client/Dtos/ReplayInfoDto.cs
@@ -5,5 +5,5 @@ public sealed class ReplayInfoDto
     public required string UploaderId { get; set; }
     public required string UploaderName { get; set; }
     public required DateTime UploadedAt { get; set; }
-    public int Position { get; set; } // position 0 is tricky because it is required but 0 is default so its trimmed and treated as not set
+    public int? Position { get; set; }
 }
